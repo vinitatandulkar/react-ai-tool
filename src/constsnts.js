7747541ba34c268077b89c693fc6c9da5f85c006
Vinita Tandulkar <vinitatandulkar29@gmail.com>
2026-02-01 15:10:50 +0530

    Api key updated

--- src/constsnts.js
+++ src/constsnts.js
@@ -1,2 +1,2 @@
 export const URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAacCUDnmnzP-Ydotjr_ulILwCQaqyXDx4";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyA2loVehho-p9_8rfU_d4KgkiQw7bSUxRQ";
