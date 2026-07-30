a729d4334f92de88ec7177387f7e7f165640e896
Vinita Tandulkar <vinitatandulkar29@gmail.com>
2026-07-30 14:10:13 +0530

    Update constsnts.js
    API key changed

--- src/constsnts.js
+++ src/constsnts.js
@@ -1,2 +1,2 @@
 export const URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDQoRTsVE6x8GojSQ3M_QA311NXJhhwn2M";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyA2loVehho-p9_8rfU_d4KgkiQw7bSUxRQ";
